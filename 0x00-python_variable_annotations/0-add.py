248be79f9b3b85deac81985db42f0ab147bbbc5e
Valentine Mahembe <mahembevalentine@gmail.com>
2024-03-14 11:59:27 +0200

    0 - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Adds 2 numbers and returns their sum.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+    Returns:
+        float: The sum of `a` and `b`.
+    """
+    return a + b
