6e38a69d7fd82b71c100876cee76c1f3d17a3d92
Valentine Mahembe <mahembevalentine@gmail.com>
2024-03-14 12:29:22 +0200

    1. Basic annotations - concat correction

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-def concat(str1: str, str2:str) -> str:
+def concat(str1: str, str2: str) -> str:
     """
     Concatenates two input strings and returns the resulting string.
 
